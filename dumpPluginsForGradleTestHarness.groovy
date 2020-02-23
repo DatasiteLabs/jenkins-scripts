410afc0864aeef800c1a4273b2723bc4e96cae9a
Kayla Altepeter <kayla@kaylaaltepeter.com>
2020-02-23 10:19:15 -0600

    Update dumpPluginsForGradleTestHarness.groovy

--- dumpPluginsForGradleTestHarness.groovy
+++ dumpPluginsForGradleTestHarness.groovy
@@ -6,6 +6,6 @@ def plugins = Jenkins.instance.pluginManager.plugins.toSorted()
 plugins.each{
   plugin ->
   def attrs = plugin.getManifest().mainAttributes
-  returnList << "dependency \'${attrs.getValue('Group-Id')}:${attrs.getValue('Short-Name')}:${attrs.getValue('Plugin-Version')}\'"
+  returnList << "dependency \'${attrs.getValue('Group-Id')}\', \'${attrs.getValue('Short-Name')}\', \'${attrs.getValue('Plugin-Version')}\'"
 }
 return returnList.join('\n').toString()
